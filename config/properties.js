00aafb512c622b072bb102719292f6a566e001b9
David Gomez <davidgd2992@gmail.com>
2019-08-19 17:05:02 -0400

    mongodb DB connection

--- config/properties.js
+++ config/properties.js
@@ -1,4 +1,4 @@
 module.exports = {
     PORT: process.env.PORT || 3000,
-    DB: 'mongodb://localhost:27017/loginm'
+    DB: 'mongodb+srv://admin:VH5QGv4x$9q_Pr8@cluster0-dizfu.mongodb.net/test?retryWrites=true&w=majority'
 }
